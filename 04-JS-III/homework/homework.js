eecca7cba35166fc611f7e2671652faf389f3a0f
Andres <andresbaquero99@hotmail.com>
2022-03-27 00:29:20 -0500

    Ejercicios resueltos

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -264,11 +264,12 @@ function continueStatement(numero) {
   // Tu código:
   var array = [];
   var suma = numero;
+  
   for(var i= 0; i<10; i++) {
     if(i === 5) continue;
     else {
-      suma = suma + 2;
-      array.push(suma);
+     suma = suma + 2;
+     array.push(suma);
     }
   }
   return array;
